283dc242223530a2a8093dc79fb2a441c301a9be
Zandi Milligan <zmilligan@ou.edu>
2019-02-14 14:04:07 -0600

    updated

--- Todo.java
+++ Todo.java
@@ -7,11 +7,11 @@ OU EMAIL____________: zmilligan@ou.edu
 Tasks:
 1. write twoToken method for Calculator
 2. write threeToken method
-3. Write execute method
-4. Write parseAndExecute method
-5. Complete twoToken tests
-6. Complete threeToken tests
-7. Complete execute tests
+3. Complete twoToken tests
+4. Complete threeToken tests
+5. Write execute method
+6. Complete execute tests
+7. Write parseAndExecute method
 8. Complete parseAndExecute tests
 
 Estimated Time (minutes):
@@ -20,14 +20,14 @@ Estimated Time (minutes):
 3. 15
 4. 15
 5. 15
-6. 15
-7. 20
+6. 20
+7. 15
 8. 20
 
 Actual Time (minutes):
-1. 
-2. 
-3. 
+1. 9
+2. 6
+3. 17
 4. 
 5. 
 6. 
